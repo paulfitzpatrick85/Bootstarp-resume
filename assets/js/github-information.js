f54033ba16b8f448dd4c5a57de67e6f4e1ca63cf
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-19 16:55:02 +0000

    render user info from search box

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,28 @@
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)  
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+/* line5-@ will appear before user login name, ${user.html_url}, link to users profile and opens in new window.
+${user.login} - text will be users login name.
+line 14 - will show user info*/
+
+
+
+
+
 function fetchGitHubInformation(event) {       //event arg used as function is called by onInput event
     var username = $("#gh-username").val();   //use JQuery to select id and value of text field
     if (!username) {                //if no username entered
@@ -8,5 +33,25 @@ function fetchGitHubInformation(event) {       //event arg used as function is c
         `<div id="loader">
         <img src="assets/css/loader.gif" alt="loading...">
         </div>`);
+
+        //when method take a function as first arg,
+        $.when(
+            $.getJSON(`https://api.github.com/users/${username}`) //value obtained from input box
+        ).then(
+            function(response) {  //1st arg is response from get.json method
+                var userData = response;
+                $("#gh-user-data").html(userInformationHTML(userData));  //set content of div to results of function userInformationHTML 
+            },
+            function(errorResponse) {
+                if (errorResponse.status === 404) {  //404 is not found error
+                    $("#gh-user-data").html(
+                        `<h2>No info found for user ${username}</h2>`); //html set to the following in case of error
+                } else {               //if not a 404 error
+                    console.log(errorResponse);
+                    $("#gh-user-data").html(
+                        `<h2>Error: ${errorResponse.responseJSON.message}</h2>`); //html set to json reponse
+                }
+            });
+
 }
 
